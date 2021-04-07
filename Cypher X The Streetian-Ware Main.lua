162894eb665d5ce2bf188af93a9bfcc9b157e314
Enviie <50255493+Enviie@users.noreply.github.com>
2021-04-07 17:02:04 -0500

    Update Cypher X The Streetian-Ware Main.lua

--- Cypher X The Streetian-Ware Main.lua
+++ Cypher X The Streetian-Ware Main.lua
@@ -1,7 +1,9 @@
 if syn or SENTINEL_V2 or pebc_create then
     loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Enviie/Cypher-X-The-Streetian-Ware/main/Cypher%20X%20The%20Streetian-Ware%20Paid%20Exploits.lua"))("Enviie Soo Hot!")
+    wait()
     warn("Using Paid Exploits")
 else
     loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Enviie/Cypher-X-The-Streetian-Ware/main/Cypher%20X%20The%20Streetian-Ware%20Free%20Exploits"))("Enviie Soo Hot!")
+    wait()
     warn("Using Free Exploits")
 end    
